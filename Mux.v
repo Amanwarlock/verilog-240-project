b1d85ee763712890e3a33a94f2b741dcefd067db
Aman Kareem <amankareem@mail.fresnostate.edu>
2020-05-06 15:43:29 -0700

    impl

--- Mux.v
+++ Mux.v
@@ -16,13 +16,13 @@ parameter PARITY = 3'b000, //0
 	  ROTR = 3'b010, //2
 	  ROTL = 3'b011; // 3 
 
-always @(posedge clk) begin
+always @(parity_out,popcount_out,rotr_out,rotl_out) begin
 	case(opcode) 
-		PARITY: alu_out <= #5 parity_out;
-		POPCOUNT: alu_out <= #5 popcount_out;
-		ROTR: alu_out <= #5 rotr_out;
-		ROTL: alu_out <= #5 rotl_out;
-		default: alu_out <= #5 0;
+		PARITY: alu_out =  parity_out;
+		POPCOUNT: alu_out = popcount_out;
+		ROTR: alu_out =  rotr_out;
+		ROTL: alu_out = rotl_out;
+		default: alu_out = 0;
 	endcase
 end
 
